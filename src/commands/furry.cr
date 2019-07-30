cdb72438889df615a1df281403410f5e44eb8bc5
GeopJr <geopjr@geopjr.xyz>
2019-07-30 14:26:30 +0300

    Added more random args + new api

--- src/commands/furry.cr
+++ src/commands/furry.cr
@@ -25,30 +25,82 @@ module Granz
             limit = 1
           end
 
-          tagged_body = {"tags" => tags, "limit" => limit}.to_json
-          response = HTTP::Client.post("https://e621.net/post/index.json", headers: HTTP::Headers{"User-Agent" => "Granz by GeopJr (geopjr@geopjr.xyz)", "Content-Type" => "application/json"}, body: tagged_body)
-          bad_status = Discord::Embed.new(
-            colour: 0xffff00,
-            title: "API returned #{response.status_code}. Please wait a bit before using!"
-          )
-          next BOT.create_message(payload.channel_id, "", bad_status) unless response.status_code == 200
-
-          value = JSON.parse(response.body)
-          nothing_found = Discord::Embed.new(
-            colour: 0xffff00,
-            title: "Nothing found for those tags!"
-          )
-          next BOT.create_message(payload.channel_id, "", nothing_found) unless value.as_a.size != 0
-          random_image = rand(0..value.as_a.size)
-          embed = Discord::Embed.new(
-            colour: 0xffff00,
-            title: "Link",
-            url: "#{value[random_image]["file_url"].as_s}",
-            image: Discord::EmbedImage.new(
-              url: "#{value[random_image]["file_url"].as_s}"
+          if tags.split(" ").size > 1 && tags.split(" ")[0].downcase == "random"
+            available_args = ["bang", "bulge", "cuddle", "group", "hug", "kiss", "lick", "suck", "gay", "straight"]
+            bad_status = Discord::Embed.new(
+              colour: 0xffff00,
+              title: "#{tags.split(" ")[1]} is not a valid arg!",
+              description: "[HERE](https://granz.geopjr.xyz/furry) are the available args!"
             )
-          )
-          BOT.create_message(payload.channel_id, "", embed)
+            next BOT.create_message(payload.channel_id, "", bad_status) unless available_args.includes?(tags.split(" ")[1].downcase)
+            base_url_furry = "https://api.furry.bot/furry/nsfw/"
+            if tags.split(" ")[1].downcase == "bang"
+              response = HTTP::Client.get "#{base_url_furry}/bang"
+              value = JSON.parse(response.body)
+            elsif tags.split(" ")[1].downcase == "bulge"
+              response = HTTP::Client.get "#{base_url_furry}/bulge"
+              value = JSON.parse(response.body)
+            elsif tags.split(" ")[1].downcase == "cuddle"
+              response = HTTP::Client.get "#{base_url_furry}/cuddle"
+              value = JSON.parse(response.body)
+            elsif tags.split(" ")[1].downcase == "group"
+              response = HTTP::Client.get "#{base_url_furry}/group"
+              value = JSON.parse(response.body)
+            elsif tags.split(" ")[1].downcase == "hug"
+              response = HTTP::Client.get "#{base_url_furry}/hug"
+              value = JSON.parse(response.body)
+            elsif tags.split(" ")[1].downcase == "kiss"
+              response = HTTP::Client.get "#{base_url_furry}/kiss"
+              value = JSON.parse(response.body)
+            elsif tags.split(" ")[1].downcase == "lick"
+              response = HTTP::Client.get "#{base_url_furry}/lick"
+              value = JSON.parse(response.body)
+            elsif tags.split(" ")[1].downcase == "suck"
+              response = HTTP::Client.get "#{base_url_furry}/suck"
+              value = JSON.parse(response.body)
+            elsif tags.split(" ")[1].downcase == "gay"
+              response = HTTP::Client.get "#{base_url_furry}/yiff/gay"
+              value = JSON.parse(response.body)
+            elsif tags.split(" ")[1].downcase == "straight"
+              response = HTTP::Client.get "#{base_url_furry}/yiff/straight"
+              value = JSON.parse(response.body)
+            else
+              response = HTTP::Client.get "#{base_url_furry}/yiff/straight"
+              value = JSON.parse(response.body)
+            end
+            furry_embed = Discord::Embed.new(
+              colour: 0xffff00,
+              image: Discord::EmbedImage.new(
+                url: "#{value["response"]["image"].as_s}"
+              )
+            )
+            BOT.create_message(payload.channel_id, "", furry_embed)
+          else
+            tagged_body = {"tags" => tags, "limit" => limit}.to_json
+            response = HTTP::Client.post("https://e621.net/post/index.json", headers: HTTP::Headers{"User-Agent" => "Granz by GeopJr (geopjr@geopjr.xyz)", "Content-Type" => "application/json"}, body: tagged_body)
+            bad_status = Discord::Embed.new(
+              colour: 0xffff00,
+              title: "API returned #{response.status_code}. Please wait a bit before using!"
+            )
+            next BOT.create_message(payload.channel_id, "", bad_status) unless response.status_code == 200
+
+            value = JSON.parse(response.body)
+            nothing_found = Discord::Embed.new(
+              colour: 0xffff00,
+              title: "Nothing found for those tags!"
+            )
+            next BOT.create_message(payload.channel_id, "", nothing_found) unless value.as_a.size != 0
+            random_image = rand(0..value.as_a.size)
+            embed = Discord::Embed.new(
+              colour: 0xffff00,
+              title: "Link",
+              url: "#{value[random_image]["file_url"].as_s}",
+              image: Discord::EmbedImage.new(
+                url: "#{value[random_image]["file_url"].as_s}"
+              )
+            )
+            BOT.create_message(payload.channel_id, "", embed)
+          end
         rescue
           embed = Discord::Embed.new(
             colour: 0xffff00,
