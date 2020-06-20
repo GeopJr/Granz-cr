8277ea4b3f85aca48b9b5538e55e14b99d93c7b4
GeopJr <geopjr@geopjr.xyz>
2020-06-20 15:05:49 +0300

    :ambulance: Now you can freely use constants in command files!

--- src/commands/collector.cr
+++ src/commands/collector.cr
@@ -2,13 +2,16 @@
 # Which will be used to auto generate the help page
 # But can also be used to auto generate a help emebed etc.
 module Granz
+  # Let's create our own record
   record Command, name : String, category : String, usage : String, example : String, desc : String, args : Bool? = false
-
+  # Commands hash that has all command info
   COMMANDS = Hash(String, Command).new
 
   module Commands
+    # Macro that lists all available constants under Granz::Commands
+    # and also filters through them
     def self.collected_modules
-      {{@type.constants}}
+      {{@type.constants}}.reject! { |x| !COMMANDS.keys.includes?(x.to_s.split("::Commands::", limit: 2)[-1].downcase) }
     end
   end
 end
