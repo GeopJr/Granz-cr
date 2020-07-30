3a4898b4ee1da526b4fb083d6ced155a08f87851
GeopJr <geopjr@geopjr.xyz>
2020-07-31 01:02:52 +0300

    :lipstick: Code block for better view

--- src/commands/utilities/donate.cr
+++ src/commands/utilities/donate.cr
@@ -8,7 +8,7 @@ module Granz
 
       def execute(payload : Discord::Message, args : Array(String))
         return false if Granz::Args.max(1, args, payload.channel_id)
-        return BOT.create_message(payload.channel_id, "", Discord::Embed.new(colour: 0xff0000, title: "Available coins:", description: "`#{DONATION_LINKS.keys.join(",")}`", footer: Discord::EmbedFooter.new(text: "Thanks for donating! Don't forget to leave your username so I can thank you personally!"))) unless DONATION_LINKS.keys.includes?(args.join(" ").upcase)
+        return BOT.create_message(payload.channel_id, "", Discord::Embed.new(colour: 0xff0000, title: "Available coins:", description: "```\n#{DONATION_LINKS.keys.join(", ")}\n```", footer: Discord::EmbedFooter.new(text: "Thanks for donating! Don't forget to leave your username so I can thank you personally!"))) unless DONATION_LINKS.keys.includes?(args.join(" ").upcase)
         
         embed = Discord::Embed.new(
           colour: 0xffff00,
