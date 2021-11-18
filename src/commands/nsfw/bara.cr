bb99fc496f906d2cbee72353ba3dcc3c3a1d37d7
GeopJr <18014039+GeopJr@users.noreply.github.com>
2021-11-18 07:11:00 +0200

    fix(bara): api 301 subdomain

--- src/commands/nsfw/bara.cr
+++ src/commands/nsfw/bara.cr
@@ -16,7 +16,7 @@ module Granz
         end
         return BOT.create_message(payload.channel_id, "", Discord::Embed.new(colour: 0xffff00, image: Discord::EmbedImage.new(url: "https://i.imgur.com/yVs6TqV.gif"), title: "I'm sorry. I can't do that because this is a SFW channel.")) unless CACHE.resolve_channel(payload.channel_id).nsfw
         i = 0
-        response = HTTP::Client.get("https://rule34.xxx/index.php?page=dapi&s=post&q=index&pid=#{rand(50)}&tags=bara%20-furry%20-furry_only%20-anthro%20-3d%20-3d_(artwork)%20-forced%20-yaoi%20-pokemon%20-canid%20-humanoid%20-humanoid_penis%20-mammal%20-animal_humanoid%20-fur%20-vagina%20-pussy")
+        response = HTTP::Client.get("https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&pid=#{rand(50)}&tags=bara%20-furry%20-furry_only%20-anthro%20-3d%20-3d_(artwork)%20-forced%20-yaoi%20-pokemon%20-canid%20-humanoid%20-humanoid_penis%20-mammal%20-animal_humanoid%20-fur%20-vagina%20-pussy")
         return BOT.create_message(payload.channel_id, "", Discord::Embed.new(colour: 0xff0000, title: "API returned #{response.status_code}")) unless response.status_code == 200
         xml = XML.parse(response.body.gsub("\n", "")).first_element_child
         embed_down = Discord::Embed.new(colour: 0xffff00, title: "An error occured", description: "Rule34.xxx might be down!\nFeel free to message us on our support server, to let us know!")
